62ecc7ec7f1ccd39d99a95c0e24747e2e46ece10
MohammedSinancholakkal <sinansibi@gmail.com>
2025-11-26 10:16:31 +0530

    fef

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -19,6 +19,28 @@
 
 
 
+// const sql = require("mssql");
+
+// const config = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   server: process.env.DB_SERVER,
+//   database: process.env.DB_NAME,
+//   options: {
+//     encrypt: true,
+//     trustServerCertificate: true 
+//   },
+// };
+
+// sql.connect(config)
+//   .then(() => console.log("Database connected successfully"))
+//   .catch((err) => console.log("DB connection failed", err));
+
+// module.exports = sql;  
+
+
+
+
 const sql = require("mssql");
 
 const config = {
