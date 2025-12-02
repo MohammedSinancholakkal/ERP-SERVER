0263684b42c64ee3df06d6add5c9092921e5dc36
MohammedSinancholakkal <sinansibi@gmail.com>
2025-12-02 18:11:30 +0530

    sh

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ ERP_SERVER.use("/api/expenses", expenseRoutes);
 
 
 // const PORT = process.env.PORT;
-// const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT;
 
 
